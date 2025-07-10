afe88237ba23ba4a6f809d886dff00d912dcd78a
Maxim Rybetsky <stsdewar@ya.ru>
2025-07-10 14:59:36 +0300

    add CandidateSaveHistoryEntity

---
+++ src/main/java/org/hrsninja/api/model/CandidateSaveHistoryEntity.java
@@ -0,0 +1,15 @@
+package org.hrsninja.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class CandidateSaveHistoryEntity {
+    private UUID id;
+    private UUID candidateId;
+    private LocalDateTime createdDatetime;
+}
